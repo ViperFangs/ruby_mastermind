68a5f48308e14baedb4e8f665da1fe7f0d7532e2
Aarya <amathrej@asu.edu>
2022-05-29 00:32:29 -0700

    Change indentation in display_instructions

--- lib/game_instructions.rb
+++ lib/game_instructions.rb
@@ -8,12 +8,12 @@ module GameInstructions
       This is a 1-player game against the computer. You can choose to be the Code Maker or Code Breaker.
 
       There are six different color combinations:
-      Red = 1
-      Green = 2
-      Blue = 3
-      Yellow = 4
-      Cyan = 5
-      Purple = 6
+        Red = 1
+        Green = 2
+        Blue = 3
+        Yellow = 4
+        Cyan = 5
+        Purple = 6
 
       The code maker will choose four to create a 'master code'. For example,
       'Red, Blue, Green, Red' can be typed as: '1 3 2 1' (Without the '')
@@ -29,13 +29,13 @@ module GameInstructions
 
       Clue Example:
       To continue the example, using the above 'master code' a guess of "1134" would produce 3 clues:
-      1 1 3 4  Clues: $ ? ?
+        1 1 3 4  Clues: $ ? ?
 
       The guess had 1 correct number in the correct location and 2 correct numbers in a wrong location.
 
       Pick Your Poison:
-      Type '1' to be the MAKER
-      Type '2' to be the BREAKER
+        Type '1' to be the MAKER
+        Type '2' to be the BREAKER
     HEREDOC
   end
 
