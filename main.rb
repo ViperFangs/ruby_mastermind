4002b57f65db7219b2ccdb381c3b57e303748dd6
Aarya <amathrej@asu.edu>
2022-05-29 00:06:56 -0700

    Require game_rules file in main.rb

--- main.rb
+++ main.rb
@@ -1,13 +1,11 @@
 # rubocop:disable Metrics/AbcSize
 
+require_relative './lib/game_rules.rb'
+
 class Mastermind
+  include GameRules
   def initialize
     display_rules
-  end
-
-  def display_rules
-    puts "\nThis is how to play Mastermind:\n\nThis is a 1-player game against the computer.\nYou can choose to be the code maker or the code breaker."
-    puts "\nThere are 6 different types of color combinations:\n"
   end
 end
 
