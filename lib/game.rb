ca43d1ff8544eb301f4dd1364e56f09abe8b2ffc
Aarya <amathrej@asu.edu>
2022-05-29 14:30:39 -0700

    Use require_relative to include GameLogic module

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,9 @@
+require_relative './game_instructions'
+require_relative './game_logic'
+
 class Game
   include GameInstructions
+  include GameLogic
 
   def initialize
     display_instructions
--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require_relative './lib/game_instructions'
 require_relative './lib/game'
+require_relative './lib/game_logic'
 
 Game.new
