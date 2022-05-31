6a80e2d11b612dd0e4936a00fadb0b2f891cf73e
Aarya <amathrej@asu.edu>
2022-05-31 02:18:42 -0700

    Change display_instructions method

--- lib/game_instructions.rb
+++ lib/game_instructions.rb
@@ -9,31 +9,28 @@ module GameInstructions
 
       This is a 1-player game against the computer. You can choose to be the Code Maker or Code Breaker.
 
-      There are six different color combinations:
-        Red = 1
-        Green = 2
-        Blue = 3
-        Yellow = 4
-        Cyan = 5
-        Purple = 6
+      Here are the color codes:
+
+      Red = 1\t\t\tYellow = 4
+      Green = 2\t\tCyan = 5
+      Blue = 3\t\tPurple = 6
 
       The code maker will choose four to create a 'master code'. For example,
-        'Red, Blue, Green, Red' can be typed as: '1321' (Without the '')
-
+      'Red, Blue, Green, Red' can be typed as: '1 3 2 1' (Without the '')
       As you can see, there can be more then one of the same color.
+
       In order to win, the code breaker needs to guess the 'master code' in 12 or less turns.
 
       Clues:
       After each guess, there will be up to four clues to help crack the code.
-
-        $ This clue means you have 1 correct number in the correct location.
-        ? This clue means you have 1 correct number, but in the wrong location.
+      $ This clue means you have 1 correct number at the correct location.
+      ? This clue means you have 1 correct number, but at the wrong location.
 
       Example:
-      To continue the example, using the above 'master code' a guess of "1134" would produce 3 clues:
-        1 1 3 4  Clues: $ ? ?
+      To continue the example, using the above 'master code' a guess of "1 1 3 4" would produce 3 clues:
+      Guess: ["1", "1", "3", "4"]    Clues: ["$", "?", "?"]
 
-      The guess had 1 correct number in the correct location and 2 correct numbers in a wrong location.
+      The guess had 1 correct number at the correct location and 2 correct numbers at a wrong location.
 
       When you are ready,
         Type '1' to be the MAKER
