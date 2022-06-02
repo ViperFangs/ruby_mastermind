accfcd8cc075385bcf212391ad2c9c80fe5ba969
Aarya <amathrej@asu.edu>
2022-06-01 20:02:19 -0700

    Develop a semi-working design

--- lib/code_maker.rb
+++ lib/code_maker.rb
@@ -1,16 +1,17 @@
 # Code Maker Gameplay Style
 module CodeMaker
   attr_accessor :user_master_code, :current_master_code, :possible_guesses, :range_array,
-                :current_computer_clue_array, :current_computer_guess
+                :current_computer_clue_array, :computed_clues, :computed_missed_array, 
+                :modified_computer_guess_array, :current_computer_guess_array
 
   def maker_gameplay
     puts display_color_numbers
     input_user_master_code
     convert_user_master_code
     clear_screen
-    puts "The Master Code is: #{user_master_code}\n"
+    puts "The Master Code is: #{master_code}\n"
     generate_possible_guesses_array
-    generate_computer_current_clue('1122')
+    generate_computer_current_clue
     remove_different_possible_guesses
 
     computer_gameplay until available_moves.zero? || winner?
@@ -30,11 +31,12 @@ module CodeMaker
   end
 
   def convert_user_master_code
-    self.user_master_code = current_master_code.delete(' ').to_i.digits.to_a.reverse
-    self.master_code = user_master_code
+    self.master_code = current_master_code.delete(' ').to_i.digits.to_a.reverse
   end
 
   def computer_gameplay
+    clear_screen
+
     puts if display_guess_clue_array
     display_available_moves
     puts
@@ -43,19 +45,45 @@ module CodeMaker
     remove_different_possible_guesses
   end
 
-  def generate_computer_current_clue(guess_string)
-    self.current_computer_guess = guess_string
-    self.current_computer_clue_array = generate_clue(guess_string)
+  def generate_computer_current_clue(guess_array = [1, 1, 2, 2])
+    self.current_computer_guess_array = guess_array
+    self.current_computer_clue_array = generate_clue(guess_array.join)
+    self.available_moves -= 1
   end
 
-  def generate_possible_guesses_array
-    self.range_array = [1, 2, 3, 4, 5, 6]
-    self.possible_guesses = range_array.repeated_permutation(4).to_a
+  def remove_different_possible_guesses
+    possible_guesses.select do |array|
+      self.computed_clues = []
+      self.computed_missed_array = []
+      self.modified_computer_guess_array = current_computer_guess_array[0..current_computer_guess_array.length]
+      computed_clue_handler(array)
+      computed_missed_array_handler
+
+      computed_clues == current_computer_clue_array
+    end
   end
 
-  # def remove_different_possible_guesse
-  #   possible_guesses.select do |array|
-  #     current_computer_clue_array.each
-  #   end
-  # end
+  def computed_clue_handler(array)
+    array.each_with_index do |number, index|
+      if current_computer_guess_array[index] == array[index]
+        computed_clues.push('$')
+        modified_computer_guess_array[index] = nil
+      else
+        computed_missed_array.push(number)
+      end
+    end
+  end
+
+  def computed_missed_array_handler
+    return if computed_missed_array.nil?
+
+    computed_missed_array.compact!
+
+    computed_missed_array.each do |number|
+      if modified_computer_guess_array.include? number
+        computed_clues.push('?')
+        modified_computer_guess_array.delete_at(modified_computer_guess_array.find_index(number))
+      end
+    end
+  end
 end
