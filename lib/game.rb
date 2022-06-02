22de26a0e64b76a68398be63204b18627fb68039
Aarya <amathrej@asu.edu>
2022-06-01 18:47:05 -0700

    Include CodeMaker module

--- lib/game.rb
+++ lib/game.rb
@@ -3,12 +3,14 @@
 require_relative './game_instructions'
 require_relative './game_logic'
 require_relative './game_display'
+require_relative './code_maker'
 
 # Game class calls different methods to display the mastermind game
 class Game
   include GameInstructions
   include GameLogic
   include GameDisplay
+  include CodeMaker
 
   attr_accessor :user_gameplay_choice
 
